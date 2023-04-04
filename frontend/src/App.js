f59db86a89ae2ae9bf59ec03ae6a622f817ad0b5
Illia <illia.horban@gmail.com>
2023-04-04 14:52:17 +0300

    nav style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
+import {Container} from 'react-bootstrap';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
-      <div>Hello</div>
-    </div>
+    <>
+      <Header/>
+          <main className='py-3'>
+            <Container>
+              <h1>Main</h1>
+            </Container>
+          </main>
+      <Footer/>
+    </>
   );
 }
 
