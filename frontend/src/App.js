9622662a634a440f604b4766aa0c9e80e9f83e02
Illia <illia.horban@gmail.com>
2023-04-05 11:18:30 +0300

    Product/ProductScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
-import {Container} from 'react-bootstrap';
-import Footer from "./components/Footer";
+import React from "react";
 import Header from "./components/Header";
-
-function App() {
-  return (
-    <>
-      <Header/>
-          <main className='py-3'>
-            <Container>
-              <h1>Main</h1>
-            </Container>
-          </main>
-      <Footer/>
-    </>
-  );
-}
-
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+ 
+const App = () => {
+    return (
+        <Router>
+            <Header />
+            <main className='py-3'>
+                <Container>
+                    <Routes>
+                        <Route path='/' element={<HomeScreen />}/>
+                        <Route path='/product/:id' element={<ProductScreen />} />
+                    </Routes>
+                </Container>
+            </main>
+            <Footer />
+        </Router>
+    );
+};
+ 
 export default App;
