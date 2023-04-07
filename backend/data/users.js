db9733d3f4f0649bab79f98685fb17ebe670cd07
Illia <illia.horban@gmail.com>
2023-04-07 17:49:14 +0300

    postman/mongoDB connected

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,3 +1,21 @@
+import bcrypt from 'bcryptjs'
+
 const users = [
-    
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true
+    },
+    {
+        name: 'Illia Horban',
+        email: 'illia@example.com',
+        password: bcrypt.hashSync('123456', 10),
+    },
+    {
+        name: 'Dan Milman',
+        email: 'dan@example.com',
+        password: bcrypt.hashSync('123456', 10),
+    },
 ]
+export default users;
