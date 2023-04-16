111d9b57f02fbf59f9f29db44e9c2e1a65622ae0
Illia <illia.horban@gmail.com>
2023-04-16 21:00:54 +0300

    login form

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 import { PRODUCT_LIST_FAIL, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_REQUEST, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL } from "../constans/productConstans";
 
-export const listProducts = () => async (disatch) => {
+export const listProducts = () => async (dispatch) => {
     try {
-        disatch({type: PRODUCT_LIST_REQUEST})
+        dispatch({type: PRODUCT_LIST_REQUEST})
 
         const {data} = await axios.get('/api/products')
 
-        disatch({
+        dispatch({
             type: PRODUCT_LIST_SUCCESS,
             payload: data
         })
     } catch (error) {
-        disatch({
+        dispatch({
             type: PRODUCT_LIST_FAIL,
             payload: error.response && error.response.data.message ? error.response.data.message  : error.message,  
         })
@@ -20,18 +20,18 @@ export const listProducts = () => async (disatch) => {
 } 
 
 
-export const listProductDetails = (id) => async (disatch) => {
+export const listProductDetails = (id) => async (dispatch) => {
     try {
-        disatch({type: PRODUCT_DETAILS_REQUEST})
+        dispatch({type: PRODUCT_DETAILS_REQUEST})
 
         const {data} = await axios.get(`/api/products/${id}`)
 
-        disatch({
+        dispatch({
             type: PRODUCT_DETAILS_SUCCESS,
             payload: data
         })
     } catch (error) {
-        disatch({
+        dispatch({
             type: PRODUCT_DETAILS_FAIL,
             payload: error.response && error.response.data.message ? error.response.data.message  : error.message,  
         })
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,14 +2,23 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'
 import {productDatailReducer, productListReducer} from './reducers/productReducers' 
 import {cartReducer} from './reducers/cartReducer'
+import { userLoginReducer } from './reducers/userReducer'
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDatailReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer
 })
-const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
+const cartItemsFromStorage = localStorage.getItem('cartItems') 
+    ? JSON.parse(localStorage.getItem('cartItems')) 
+    : [];
+const userInfoFromStorage = localStorage.getItem('userInfo') 
+    ? JSON.parse(localStorage.getItem('userInfo')) 
+    : null;
+
 const initialState = {
-    cart: { cartItems: cartItemsFromStorage}
+    cart: { cartItems: cartItemsFromStorage},
+    userLogin: {userInfo: userInfoFromStorage}
 }
 const store = configureStore({
     reducer,
